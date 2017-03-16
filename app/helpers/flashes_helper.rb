d0d8e8d5393a4494ae5891d5c455a612fe28b5ac
dongmingjuan <d387918156@gmail.com>
2017-03-16 11:15:40 +0800

    add bootsrap flash funcion

---
+++ app/helpers/flashes_helper.rb
@@ -0,0 +1,12 @@
+module FlashesHelper
+ FLASHES_CLASS = { alert: "danger", notice: "success", warning: "warning"}.freeze
+
+ def falsh_class(key)
+   FLASHES_CLASSES.fetch key.to_sym, key
+ end
+
+ def user_facing_flashes
+   flash.to_hash.slice "alert", "notice", "warning"
+ end
+
+ end
